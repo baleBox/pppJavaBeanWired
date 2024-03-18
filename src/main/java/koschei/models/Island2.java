d7d61aa13b2f5a879a654095e2bdd11a4f1c0bba
Alexander Stupin <astupin2012@gmail.com>
2024-03-18 16:01:01 +0300

    Done! First commit.

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -2,14 +2,14 @@ package koschei.models;
 
 public class Island2 {
 
-    private Wood3 wood;
+    private final Wood3 WOOD;
 
     public Island2(Wood3 wood) {
-        this.wood = wood;
+        this.WOOD = wood;
     }
 
     @Override
     public String toString() {
-        return ", на острове дерево " + wood.toString();
+        return ", на острове дерево " + WOOD.toString();
     }
 }
